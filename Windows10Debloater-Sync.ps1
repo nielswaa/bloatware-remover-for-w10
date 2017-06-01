4e28fbb52d28d59b3a11b07ed0b6e81898b9cd9d
nielswaa <nielswaa30@gmail.com>
2017-06-02 00:16:25 +0200

    Add files via upload

---
+++ Windows10Debloater-Sync.ps1
@@ -0,0 +1,110 @@
+﻿#This utility removes all of the Windows 10 bloatware that is preinstalled. Use the # to comment
+#out a bloatware app that you would like to keep.
+
+
+#This checks if you are an administrator, and if not, then launches administrator mode to run the script.
+If (-NOT([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))
+{
+$arguments = "&" + $MyInvocation.MyCommand.Definition + "" 
+Start-Process PowerShell -Verb RunAs -ArgumentList $arguments
+Break
+}
+
+$apps = @(
+
+# default Windows 10 apps
+    "Microsoft.3DBuilder"
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.Appconnector"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"
+    #"Microsoft.WindowsCamera"
+    #"Microsoft.FreshPaint"
+    "Microsoft.Getstarted"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.Office.OneNote"
+    "Microsoft.OneConnect"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    "Microsoft.StorePurchaseApp"
+    #"Microsoft.Windows.Photos"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsCalculator"
+    "Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"
+    "Microsoft.WindowsSoundRecorder"
+    #"Microsoft.WindowsStore"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.Wallet"
+    "Microsoft.MinecraftUWP"
+    "Microsoft.NetworkSpeedTest"
+    "Microsoft.MicrosoftPowerBIForWindows"
+    "AdobeSystemsIncorporated.AdobePhotoshopExpress"
+    
+
+    # Threshold 2 apps
+    "Microsoft.CommsPhone"
+    "Microsoft.ConnectivityStore"
+    "Microsoft.Messaging"
+    "Microsoft.Office.Sway"
+    "Microsoft.OneConnect"
+    "Microsoft.WindowsFeedbackHub"
+
+
+    #Redstone apps
+    "Microsoft.BingFoodAndDrink"
+    "Microsoft.BingTravel"
+    "Microsoft.BingHealthAndFitness"
+    "Microsoft.WindowsReadingList"
+
+    # non-Microsoft
+    "9E2F88E3.Twitter"
+    "PandoraMediaInc.29680B314EFC2"
+    "Flipboard.Flipboard"
+    "ShazamEntertainmentLtd.Shazam"
+    "king.com.CandyCrushSaga"
+    "king.com.CandyCrushSodaSaga"
+    "king.com.*"
+    "ClearChannelRadioDigital.iHeartRadio"
+    "4DF9E0F8.Netflix"
+    "6Wunderkinder.Wunderlist"
+    "Drawboard.DrawboardPDF"
+    "2FE3CB00.PicsArt-PhotoStudio"
+    "D52A8D61.FarmVille2CountryEscape"
+    "TuneIn.TuneInRadio"
+    "GAMELOFTSA.Asphalt8Airborne"
+    "TheNewYorkTimes.NYTCrossword"
+    "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+    "Facebook.Facebook"
+    "flaregamesGmbH.RoyalRevolt2"
+    "Playtika.CaesarsSlotsFreeCasino"
+    "Eclipse.Manager"
+    "Code.Writer"
+)
+
+ForEach ($app in $apps) {
+    echo "Trying to remove $app"
+
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+
+    Get-AppXProvisionedPackage -Online |
+        where DisplayName -EQ $app |
+        Remove-AppxProvisionedPackage -Online 
+} 
+
+#Prevents bloatware applications from returning(Might be forced back after Win Update)
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content"
+Mkdir $registryPath
+New-ItemProperty $registryPath -Name DisableWindowsConsumerFeatures -Value 1
+
